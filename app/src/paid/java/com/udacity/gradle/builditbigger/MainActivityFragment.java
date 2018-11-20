34b7643820d3803f2fe1412e1821681b3f5fca7c
abanob-atef-roshdy <abanobatef216@gmail.com>
2018-11-20 21:57:33 +0200

    asd

--- app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,12 +1,10 @@
-package com.bebo.gradle.builditbigger.builditbigger;
+package com.udacity.gradle.builditbigger;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.udacity.gradle.builditbigger.R;
 
 public class MainActivityFragment extends Fragment {
     public MainActivityFragment() {
